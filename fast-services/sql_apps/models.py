f70a645f8e091c716195d8942c9a2b8e322763f1
KhuongNQ <khuongnqdev@gmail.com>
2024-02-19 16:18:42 +0700

    update

--- fast-services/sql_apps/models.py
+++ fast-services/sql_apps/models.py
@@ -8,7 +8,7 @@ from .database import Base
 class UserDb(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[str] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(default=True)
@@ -17,9 +17,7 @@ class UserDb(Base):
 class ProfileDb(Base):
     __tablename__ = "profiles"
 
-    id: Mapped[str] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    phone: Mapped[str] = mapped_column(nullable=False)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-
+    id: Mapped[str] = mapped_column(ForeignKey("users.id"), primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=True)
+    phone: Mapped[str] = mapped_column(nullable=True)
     user: Mapped[UserDb] = relationship("UserDb")
